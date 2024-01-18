e0684eb12ce1672e206000863ef121be3c870538
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2024-01-18 18:24:58 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,12 +3,12 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-        './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
-        './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
         './html/LoginModal.html': path.resolve(__dirname, 'template/js/custom-js/components/LoginModal.html'),
         './html/TheAccount.html': path.resolve(__dirname, 'template/js/custom-js/components/TheAccount.html'),
         './js/TheAccount.js': path.resolve(__dirname, 'template/js/custom-js/components/TheAccount.js'),
         './js/LoginBlock.js': path.resolve(__dirname, 'template/js/custom-js/components/LoginBlock.js'),
+        './js/AccountForm.js': path.resolve(__dirname, 'template/js/custom-js/components/AccountForm.js'),
+        './html/AccountForm.html': path.resolve(__dirname, 'template/js/custom-js/components/AccountForm.html'),
     }
   }
 })
